c80fb3e2bdc6e7b1db3babdcd411bf1098447e01
bgreenwell <greenwell.brandon@gmail.com>
2025-06-23 22:03:59 -0400

    fix checksums

--- Formula/lstr.rb
+++ Formula/lstr.rb
@@ -10,10 +10,10 @@ class Lstr < Formula
   # Define URLs and checksums for both architectures
   if Hardware::CPU.intel?
     url "https://github.com/bgreenwell/lstr/releases/download/v0.2.1/lstr-macos-x86_64.tar.gz"
-    sha256 "b87d6c22cf21a63070882fca6313af74b9fb001597500f669187e02bb6e667e8"
+    sha256 "554108574d4de9cfe9207dd3937e9b9d3df2b4cadfb66a9d2c4376bbff6a25df"
   else
     url "https://github.com/bgreenwell/lstr/releases/download/v0.2.1/lstr-macos-arm64.tar.gz"
-    sha256 "ff95c4991435d919583f28e40191ebfe752612b57df05ee058114920d0162c64"
+    sha256 "6b7fae4229d3d317defc774b9de5d079fb439fa2c40bb1e01b4099e182a8680c"
   end
 
   def install
