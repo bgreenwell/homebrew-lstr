aad5cbb4fe5a2634ae6cdef837461d034c5aa8da
bgreenwell <greenwell.brandon@gmail.com>
2025-06-23 20:57:43 -0400

    update urls

--- lstr.rb
+++ lstr.rb
@@ -4,16 +4,16 @@ class Lstr < Formula
   desc "A fast, minimalist directory tree viewer"
   homepage "https://github.com/bgreenwell/lstr"
   # This version will be updated automatically by our release workflow
-  version "0.2.0" # This should match the latest release
+  version "0.2.1" # This should match the latest release
   license "MIT"
 
   # Define URLs and checksums for both architectures
   if Hardware::CPU.intel?
-    url "https://github.com/bgreenwell/lstr/releases/download/v0.2.0/lstr-macos-x86_64.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    url "https://github.com/bgreenwell/lstr/releases/download/v0.2.1/lstr-macos-x86_64.tar.gz"
+    sha256 "b87d6c22cf21a63070882fca6313af74b9fb001597500f669187e02bb6e667e8"
   else
-    url "https://github.com/bgreenwell/lstr/releases/download/v0.2.0/lstr-macos-arm64.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    url "https://github.com/bgreenwell/lstr/releases/download/v0.2.1/lstr-macos-arm64.tar.gz"
+    sha256 "ff95c4991435d919583f28e40191ebfe752612b57df05ee058114920d0162c64"
   end
 
   def install
